9779bcbf8e75a1a352029fa4369bcb59dee1e58f
Thomas Pfiffner <Thomas.Pfiffner@fincon.eu>
2016-10-25 16:29:53 +0200

    comments renamed to english

--- src/main/java/de/hallosoap/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.7 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2016.10.24 um 03:16:49 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://hallosoap.de", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package de.hallosoap;
